c06a7fa472bc6f89c995ab543b7b5019a2cf80a2
Tee <tessmcandrew@gmail.com>
2014-07-23 20:38:07 -0400

    #dowerk

---
+++ teemcandrew/customizing_APIS/scripts.js
@@ -0,0 +1,28 @@
+console.log('TESTING');
+
+var instagramClientID = { '841bf62d73a24a2588ad893ed1a7fa00'
+$.ajax();
+	type: 'GET'
+	dataType: 'jsonp',
+	url: 'https://api.instagram.com/v1/media/search?/client_id=841bf62d73a24a2588ad893ed1a7fa00',
+		+ instagramClientID
+		+ '&lat=43'
+		+ '&lng=-79.4'
+	success: function('response'),
+		console.log(response);
+		response.data.forEach(function(photo))
+			console.log
+		});
+	};
+	
+};
+
+$(document). ready(function() {
+	var mapContainer = $('#mapContainer')[0];
+	map = new google.maps
+	console.log('document is ready');
+
+});
+
+console.log('testing');
+
